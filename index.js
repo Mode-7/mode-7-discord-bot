0096577507e2e534ae28fbd4e8745942d20313c8
Luis Carlos Pando <hey@luiscarlospando.com>
2021-01-31 23:53:23 -0700

    Agregar comando 'anunciar'

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 
 const client = new Discord.Client();
+const webhookClient = new Discord.WebhookClient(config.webhookID, config.webhookToken);
 
 const prefix = config.prefix;
 
+client.once("ready", () => {
+    console.log("Listo para la acción.");
+});
+
 client.on("message", function (message) {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+    if (!message.member.roles.cache.has(config["announcer-role"]) || !message.content.startsWith(prefix) || message.author.bot) return;
 
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
@@ -16,7 +20,13 @@ client.on("message", function (message) {
     if (command === "hola") {
         const timeTaken = Date.now() - message.createdTimestamp;
         message.reply(`¡Holi! Me tomó ${timeTaken}ms darme cuenta de lo guapo que estás, bombón.`);
-    }                        
+    } else if (command == "anunciar") {
+        var announcement = "";
+        for (const word in args) {
+            announcement = announcement + args[word] + " ";
+        }
+        webhookClient.send(announcement)
+    }                  
 });
 
 client.login(config.token);
