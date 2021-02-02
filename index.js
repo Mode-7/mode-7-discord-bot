42f6b6f73c378754cddf82261d6166a5cdc71205
Luis Carlos Pando <hey@luiscarlospando.com>
2021-02-02 10:09:17 -0700

    Crear método Press F to pay Respects

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ client.on("message", function (message) {
     const command = message.content;
 
     if (command === "f" || command === "F") {
-        message.channel.send(`${member.id} has paid respect.`);
+        message.channel.send(`X has paid respect.`);
     }
 });
 
