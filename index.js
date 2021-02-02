1aec2b3d49e8983dbc0e143c9b222969ce122435
Luis Carlos Pando <hey@luiscarlospando.com>
2021-02-02 09:26:47 -0700

    Separar funciones en archivos propios

--- index.js
+++ index.js
@@ -1,60 +1,20 @@
 const Discord = require("discord.js");
 // const config = require("./config.json"); // No necesitamos este archivo al hacer deply en Heroku
 
+const welcome = require('./welcome');
+const commands = require('./commands');
+
 const client = new Discord.Client();
 const webhookClient = new Discord.WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
-welcomeChannelID = '741028008361721866'; // Canal de bienvenida
-rulesChannelID = '479304179102384128'; // Canal de reglas
-generalChannelID = '478782494666129419'; // Canal general
 
 const prefix = process.env.PREFIX;
 
 client.once("ready", () => {
     console.log("Estoy listo.");
-});
 
-client.on("guildMemberAdd", function (member) {
-    console.log(member);
+    welcome(client);
 
-    const welcomeText = `¡Bienvenid@  <@${member.id}>! 🎉 🤗\nAntes de comenzar, te pedimos leer las ${member.guild.channels.cache.get(rulesChannelID).toString()} y si necesitas ayuda con algo, puedes preguntar en ${member.guild.channels.cache.get(generalChannelID).toString()}.\n\nDisfruta de tu estancia en Mode 7.`
-    const channel = member.guild.channels.cache.get(welcomeChannelID);
-
-    channel.send(welcomeText);
-});
-
-client.on("message", function (message) {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
-
-    function comandosPublicos() {
-        if (command === "hola") {
-            const timeTaken = Date.now() - message.createdTimestamp;
-            message.reply(`¡Holi! Me tomó ${timeTaken}ms darme cuenta de lo guapo que estás, bombón.`);
-        } else if (command == "uwu") {
-            message.reply(`UwU`);
-        }
-    }
-
-    function comandosPrivados() {
-        if (command == "anunciar") {
-            var announcement = "";
-            for (const word in args) {
-                announcement = announcement + args[word] + " ";
-            }
-            webhookClient.send(announcement)
-        }
-    }
-
-    if (!message.member.roles.cache.has(process.env["ANNOUNCER_ROLE"])) {
-        comandosPublicos();
-    } else {
-        comandosPublicos();
-        comandosPrivados();
-    }               
+    commands(client);
 });
 
 // client.login(config.token);
