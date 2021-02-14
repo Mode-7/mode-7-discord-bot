11f27cf955e03cafd8feaabb39e16bd123ec55f3
Luis Carlos Pando <hey@luiscarlospando.com>
2021-02-14 12:12:43 -0700

    Corregir fras priísta

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ client.on("message", (message) => {
         "Lord Jugón",
         "jugón se nace, no se hace",
         "pura frase jugona ustedes, se pasan chavos",
-        "ahora si que el <:pri:733044345262964837> robó más"
+        "ahora si que el <:pri:733044345262964837> porteó más"
     ];
 
     const responseJugon = Math.floor(Math.random() * respuestasJugon.length);
