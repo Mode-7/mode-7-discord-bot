e878a8a1f484d47ad734da7d5185ce41c7f847a7
Luis Carlos Pando <hey@luiscarlospando.com>
2022-06-02 10:20:19 -0600

    Agregar comando ni-saben-leer

--- index.js
+++ index.js
@@ -132,6 +132,13 @@ client.once("ready", async () => {
         },
     });
 
+    await getApp(guildId).commands.post({
+        data: {
+            name: 'ni-saben-leer',
+            description: 'Cuando no lee la raza, usa este comando.'
+        },
+    });
+
     client.ws.on('INTERACTION_CREATE', async (interaction) => {
         const command = interaction.data.name.toLowerCase();
 
@@ -174,6 +181,9 @@ client.once("ready", async () => {
                 break;
             case "policiajugona-2":
                 reply(interaction, `https://i.imgur.com/jRYhz7i.png`);
+                break;
+            case "ni-saben-leer":
+                reply(interaction, `https://i.imgur.com/k0RNA5A.png`);
                 break;
             default:
         }
